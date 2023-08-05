2e9d68996b7f37b8316d73d07cbb1f465dde3300
misteryfors <47424667+misteryfors@users.noreply.github.com>
2023-08-06 02:59:05 +0300

    Add files via upload

--- shop/src/components/universalModal.jsx
+++ shop/src/components/universalModal.jsx
@@ -1,28 +1,37 @@
-import React from 'react';
-import '../components/css/modal.css'
-import {NavLink, Outlet, useParams} from "react-router-dom";
-import "../components/css/fix.css"
-import {useDispatch, useSelector} from "react-redux";
+    import React from 'react';
+    import '../components/css/modal.css'
+    import {NavLink, Outlet, useParams} from "react-router-dom";
+    import "../components/css/fix.css"
+    import {useDispatch, useSelector} from "react-redux";
 
 
 
 
 
-const UniversalModal = ({active,setActive,trueBtn,falseBtn,trueFunction,falseFunction,trueLink,falseLink,message})=>{
-    const dispatch = useDispatch()
-    return(
-        <div className={active ? 'Modal active': 'ModalYesNo'} onClick={()=>setActive(false)}>
-            <div className={"Modal__contentYesNo"} onClick={e=> e.stopPropagation()}>
-                <div className='YesNoContent'>
-                    {message}
-                </div>
+    const UniversalModal = ({active,setActive,trueBtn,falseBtn,trueFunction,falseFunction,trueLink,falseLink,message})=>{
+        const dispatch = useDispatch()
+        const handleKeyDown = (e) => {
 
-                <div className='YesNoContentBtn'>
-                    <NavLink to={trueLink}><button className={'TrueBtn'} onClick={trueFunction}>Да</button></NavLink>
-                    <NavLink to={falseLink}><button className={'FalseBtn'} onClick={falseFunction}>Нет</button></NavLink>
+
+            if (e.keyCode === 13) {
+                // Prevent default behavior (form submission) on Enter key press
+                e.preventDefault();
+            }
+        };
+        return(
+            <div className={active ? 'Modal active' : 'ModalYesNo'} onClick={() => setActive(false)}>
+                <div className={"Modal__contentYesNo"} onClick={e => e.stopPropagation()} onKeyDown={handleKeyDown}>
+                    <div className='YesNoContent'>
+                        {message}
+                    </div>
+
+                    <div className='YesNoContentBtn'>
+                        <NavLink to={falseLink}><button className={'FalseBtn'} onClick={falseFunction}>Нет</button></NavLink>
+                        <NavLink to={trueLink}><button className={'TrueBtn'} onClick={trueFunction}>Да</button></NavLink>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-}
-export default UniversalModal;
+        );
+    };
+
+    export default UniversalModal;
